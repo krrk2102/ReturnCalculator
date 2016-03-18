5b8a094abeb8840e9b0b00256d010b8779a03ca9
krrk2102 <wushangqi@gmail.com>
2016-03-18 15:10:06 -0400

    Well commented. makefile and sample in/output file provided.

--- ReturnCalculator/MonthlyData.cpp
+++ ReturnCalculator/MonthlyData.cpp
@@ -10,7 +10,25 @@
 
 using namespace std;
 
+/**
+ *
+ * This cpp file contains full codes that implement all member functions
+ * of MonthlyData class.
+ *
+ * @author Shangqi Wu
+ *
+ */
 
+/**
+ *
+ * This is default constructor specifying year and month information.
+ *
+ * @param y: a string represents current year.
+ * @param m: a string represents current month.
+ *
+ * Other class members are initialized to empty or zero.
+ *
+ */
 MonthlyData::MonthlyData(const string& y, const string& m) {
 	
 	year = y;
@@ -23,7 +41,16 @@ MonthlyData::MonthlyData(const string& y, const string& m) {
 
 }
 
-
+/**
+ *
+ * This is the copy constructor of MonthlyData.
+ *
+ * @param copy: input object to be fully copied.
+ *
+ * All class members are initialized to have identical values
+ * as the input object members.
+ *
+ */
 MonthlyData::MonthlyData(const MonthlyData& copy) {
 	
 	year = copy.year;
@@ -34,11 +61,28 @@ MonthlyData::MonthlyData(const MonthlyData& copy) {
 	bottomTenPercentAverage = copy.bottomTenPercentAverage;
 	monthAverage = copy.monthAverage;
 	
+	stockReturns = copy.stockReturns;
+	
+	topTenPercent = copy.topTenPercent;
+	bottomTenPercent = copy.bottomTenPercent;
+	
 }
 
 
+/**
+ *
+ * This method is private. While the program iterating through the hash table containing
+ * stock symbols, it maintains and generates min and max heaps, which correspond to top
+ * and bottom ten percent return stocks respectively. 
+ *
+ * This method is not supposed to receive any input parameters nor to return any results.
+ * This method is supposed to be called after all stock return rates have been recorded 
+ *		in the hash table class member.
+ *
+ */
 void MonthlyData:: sort() {
 	
+	// Before starting heap generation, make sure 2 heaps are empty.
 	while (!topTenPercent.empty()) {
 		topTenPercent.pop();
 	}
@@ -47,10 +91,18 @@ void MonthlyData:: sort() {
 		bottomTenPercent.pop();
 	}
 	
+	// Calculate the proper size (10% of total stocks) of heaps.
 	tenPercentSize = stockReturns.size() / 10;
-	for (const auto& sample : stockReturns
-		 ) {
+	
+	// Maintain the max and min heap while iterating the hash table.
+	// Each sample is a pair of string, or stock symbol, and double, or return rate.
+	for (const auto& sample : stockReturns) {
 		
+		// Min heap maintenance: if it is under-sized, add new sample into the heap.
+		// If it is over-sized and new sample has a larger return than the value on
+		// heap top, the top will be replaced by new sample, otherwise new sample
+		// will be discarded.
+		// All return rates in the heap will be the larger ten percent.
 		if (topTenPercent.size() < tenPercentSize) {
 			topTenPercent.push(DataSample(sample));
 		} else {
@@ -63,6 +115,8 @@ void MonthlyData:: sort() {
 			}
 		}
 		
+		// Similar to min heap maintenance, the only difference is that it keeps a
+		// max heap, so comparator is inversed.
 		if (bottomTenPercent.size() < tenPercentSize) {
 			bottomTenPercent.push(DataSample(sample));
 		} else {
@@ -79,9 +133,26 @@ void MonthlyData:: sort() {
 }
 
 
+/**
+ *
+ * This method extracts top ten percent return rates from the min heap one by one,
+ * for each data sample, or pair of stock symbol and return rate, it refer to next month
+ * data and append next month return rate to recording vector/array.
+ * This method is supposed to be called after the sort() method is executed.
+ * 
+ * @param nextMonth: an iterator, specifying address of next month data in the STL linked list.
+ *
+ * No value is supposed to be returned.
+ *
+ */
 void MonthlyData:: getTopTenPercentReturns(const list<MonthlyData>::iterator& nextMonth) {
 	
+	// Make sure recording vector/array is empty.
 	topTenPercentReturns.clear();
+	
+	// Extracting heap elements by one, and retrieve next month return of
+	// current stock, then append the next month return to the record.
+	// If all elements in the heap are all processed, exit this loop.
 	while (!topTenPercent.empty()) {
 		
 		string symbol = topTenPercent.top().corpSymbol;
@@ -94,9 +165,26 @@ void MonthlyData:: getTopTenPercentReturns(const list<MonthlyData>::iterator& ne
 }
 
 
+/**
+ *
+ * This method extracts bottom ten percent return rates from the max heap one by one,
+ * for each data sample, or pair of stock symbol and return rate, it refer to next month
+ * data and append next month return rate to recording vector/array.
+ * This method is supposed to be called after the sort() method is executed.
+ *
+ * @param nextMonth: an iterator, specifying address of next month data in the STL linked list.
+ *
+ * No value is supposed to be returned.
+ *
+ */
 void MonthlyData::getBottomTenPercentReturn(const list<MonthlyData>::iterator& nextMonth) {
 	
+	// Make sure recording vector/array is empty.
 	bottomTenPercentReturns.clear();
+	
+	// Extracting heap elements by one, and retrieve next month return of
+	// current stock, then append the next month return to the record.
+	// If all elements in the heap are all processed, exit this loop.
 	while (!bottomTenPercent.empty()) {
 		
 		string symbol = bottomTenPercent.top().corpSymbol;
@@ -109,13 +197,26 @@ void MonthlyData::getBottomTenPercentReturn(const list<MonthlyData>::iterator& n
 }
 
 
+/**
+ *
+ * This method is inspector to get year information.
+ *
+ * @return a string representing year.
+ *
+ */
 string MonthlyData::getYear() const {
 	
 	return year;
 	
 }
 
-
+/**
+ *
+ * This method is inspector to get month information.
+ *
+ * @return a string representing month.
+ *
+ */
 string MonthlyData::getMonth() const {
 	
 	return month;
@@ -123,34 +224,77 @@ string MonthlyData::getMonth() const {
 }
 
 
+/**
+ *
+ * This method is inspector to get year and month information combined.
+ *
+ * @return a string representing year and month in format of "16-Mar".
+ *
+ */
 string MonthlyData::getYearMonth() const {
 	
 	return this->getYear() + "-" + this->getMonth();
 	
 }
 
-
+/**
+ *
+ * This method is inspector to get the return rate of this month by specified 
+ * input stock symbol.
+ *
+ * @param symbol: a string of company stock symbol.
+ *
+ * @return a string representing month.
+ *
+ */
 double MonthlyData::getSingleReturn(const string& symbol) {
 	
 	return stockReturns[symbol];
 	
 }
 
-
+/**
+ *
+ * This method is inspector to get calculated average next month's return rate 
+ * of top ten percent stocks in this month. 
+ * This method is supposed to be called after sorting method and heap processing
+ * method is finished.
+ *
+ * @return a double which is the value of average return rate.
+ *
+ */
 double MonthlyData::getTopTenPercentReturn() const {
 	
 	return topTenPercentAverage;
 	
 }
 
-
+/**
+ *
+ * This method is inspector to get calculated average next month's return rate
+ * of bottom ten percent stocks in this month.
+ * This method is supposed to be called after sorting method and heap processing
+ * method is finished.
+ *
+ * @return a double which is the value of average return rate.
+ *
+ */
 double MonthlyData::getBottomTenPercentReturn() const {
 	
 	return bottomTenPercentAverage;
 	
 }
 
-
+/**
+ *
+ * This method is inspector to get calculated average next month's return rate
+ * of both top and bottom ten percent stocks in this month.
+ * This method is supposed to be called after sorting method and heap processing
+ * method is finished.
+ *
+ * @return a double which is the value of average return rate.
+ *
+ */
 double MonthlyData::getMonthReturn() const {
 	
 	return monthAverage;
@@ -158,6 +302,19 @@ double MonthlyData::getMonthReturn() const {
 }
 
 
+/**
+ *
+ * This method is a mutator which insert new stock symbol along with its current monthly 
+ * return rate into the hash table.
+ * This method is supposed to be called during the monthly data construction stage. 
+ * If this method is called, i.e., stock information is modified, after a sorting 
+ * and heap analysis process, please execute the analysis process again. 
+ * This method is not supposed to return any value.
+ *
+ * @param symbol: a string of stock company symbol.
+ * @param rate: a double number contains value of return rate in current month.
+ *
+ */
 void MonthlyData::addData(const string& symbol, const double& rate) {
 	
 	stockReturns[symbol] = rate;
@@ -165,28 +322,37 @@ void MonthlyData::addData(const string& symbol, const double& rate) {
 }
 
 
+/**
+ *
+ * This method is designed as a wrapper process of heap generating, analysis and average 
+ * values calculating. It calls private methods for heap process in proper order, so improper
+ * execution sequence by users can be prevented. 
+ * This function is supposed to be called when all stock return rates are inserted into hash table. 
+ *
+ */
 double MonthlyData::getMonthReturn(const list<MonthlyData>::iterator& nextMonth) {
 	
+	// Calls to private methods.
 	this->sort();
 	this->getTopTenPercentReturns(nextMonth);
 	this->getBottomTenPercentReturn(nextMonth);
 	
+	// Calculating top ten percentage next month's average return rate.
 	for (const double& rate : topTenPercentReturns) {
 		topTenPercentAverage += rate;
 	}
 	topTenPercentAverage /= static_cast<double>(tenPercentSize);
 	
+	// Calculating bottom ten percentage next month's average return rate.
 	for (const double& rate : bottomTenPercentReturns) {
 		bottomTenPercentAverage += rate;
 	}
 	bottomTenPercentAverage /= static_cast<double>(tenPercentSize);
 	
+	// Calculate the average of top ten and bottom ten percent next month's return rate. 
 	monthAverage = bottomTenPercentAverage - topTenPercentAverage;
 	
 	return monthAverage;
 }
-
-
-
 
 
--- ReturnCalculator/MonthlyData.h
+++ ReturnCalculator/MonthlyData.h
@@ -17,79 +17,140 @@
 
 using namespace std;
 
+/**
+ *
+ * Return Calculator class header code:
+ *
+ * This header file defines class skeleton and
+ * data structure to be used by main program.
+ *
+ * @author Shangqi Wu
+ *
+ */
 
+
+/**
+ *
+ * This struct is pair of company symbol and monthly return rate,
+ * which are stored as strings, and double numbers, respectively.
+ *
+ */
 struct DataSample {
 	
+	// Keep company symbols with corresponding monthly return together.
 	string corpSymbol;
 	double returnRate;
 	
+	// Default constructor.
 	DataSample() :
 	corpSymbol(""), returnRate(0) {}
 	
-	DataSample(const string& s, const double& rate) :
-	corpSymbol(s), returnRate(rate) {}
+	// Constructor with separate input value of symbol and return rate.
+	DataSample(const string& symbol, const double& rate) :
+	corpSymbol(symbol), returnRate(rate) {}
 	
+	// Constructor with integrate pair of symbol and return rate.
 	DataSample(const pair<string, double>& sample) :
 	corpSymbol(sample.first), returnRate(sample.second) {}
 	
 };
 
-
+/**
+ *
+ * This class stores the return rates in the same month of all companies. 
+ *
+ * Year and month indicate date.
+ * The hash table stores return rates of companies identified by company symbol.
+ * Two priority queues are used to keep track of stocks that have a return rate of 
+ *		top ten percent or bottom ten percent in this month.
+ * Two vectors are used to store next month's return rates, which correspond to top
+ *		or bottom ten percent in this month.
+ *
+ */
 class MonthlyData {
+	
 private:
 	
+	// This is the function object to help maintianing the max heap which
+	// keeps track of bottom ten percent return rates.
 	struct maxHeapComparator {
 		bool operator () (const DataSample& smaller, const DataSample& larger) const {
 			return smaller.returnRate < larger.returnRate;
 		}
 	};
 	
+	// This is the function object to help maintaining the min heap which
+	// keeps track of top ten percent return rates.
 	struct minHeapComparator {
 		bool operator () (const DataSample& smaller, const DataSample& larger) const {
 			return larger.returnRate < smaller.returnRate;
 		}
 	};
 	
+	// Year and month information.
 	string year;
 	string month;
 	
+	
+	// The hash table to store return rates of all stocks in the same month,
+	// return rate values are identified by stock symbol, the string.
+	// This variable acts as database of this month's data, which is core of functionality. 
 	unordered_map<string, double> stockReturns;
 	
+	
+	// The min and max heaps to keep track of top and bottom ten percent return rate stocks.
+	// The long variable is calculated proper size of heap size.
 	unsigned long tenPercentSize;
 	priority_queue<DataSample, vector<DataSample>, minHeapComparator> topTenPercent;
 	priority_queue<DataSample, vector<DataSample>, maxHeapComparator> bottomTenPercent;
 	
+	
+	// Retrieved next month's return rates which correspond to top or bottom ten percent
+	// return rates in this month.
 	vector<double> topTenPercentReturns;
 	vector<double> bottomTenPercentReturns;
 	
+	// Calculated return results.
 	double topTenPercentAverage;
 	double bottomTenPercentAverage;
 	double monthAverage;
 	
-	
+	// The method generates max and min heap, or the bottom and top ten percent
+	// return rate stocks list.
 	void sort();
+	
+	// This method is to retrieve next month's return values correspond to stocks of
+	// top ten percent return rates in this month.
 	void getTopTenPercentReturns(const list<MonthlyData>::iterator& nextMonth);
+	
+	// This method is similar to previous one, which returns values correspond to
+	// stocks of bottom ten percent return rates in this month.
 	void getBottomTenPercentReturn(const list<MonthlyData>::iterator& nextMonth);
 	
 public:
 	
-	
+	// A default constructor specifying year and month.
 	MonthlyData(const string& y, const string& m);
+	// The copy constructor.
 	MonthlyData(const MonthlyData& copy);
 	
-	
+	// Inspectors for date information.
 	string getYear() const;
 	string getMonth() const;
 	string getYearMonth() const;
 	
-	
+	// Inspector to retrieve stock return value of input company symbol.
 	double getSingleReturn(const string& symbol);
+	
+	// Inspectors for calculated average return rates.
 	double getTopTenPercentReturn() const;
 	double getBottomTenPercentReturn() const;
 	double getMonthReturn() const;
 	
-	
+	// The mutator to insert stock symbol and return rate into hash table.
 	void addData(const string& symbol, const double& rate);
+	
+	// A wrapper method to generate 2 heaps first, and calulate average return values.
 	double getMonthReturn(const list<MonthlyData>::iterator& nextMonth);
 	
 };
